8b468f0f326e5fde74e9694518697396277b8a51
DmitrK0v <dislakoval@gmail.com>
2022-08-27 21:12:34 +0300

    Add exchange BTC & UAH add new test

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 import traceback
+import importlib
 
 import requests
 
@@ -11,27 +12,33 @@ fh.setLevel(LOGGER_CONFIG["level"])
 fh.setFormatter(LOGGER_CONFIG["formatter"])
 
 
+def update_rate(from_currency, to_currency):
+    xrate = XRate.select().where(XRate.from_currency == from_currency,
+                                 XRate.to_currency == to_currency).first()
+
+    module = importlib.import_module(f"api.{xrate.module}")
+    module.Api().update_rate(xrate)
+
+
 class _Api:
     def __init__(self, logger_name):
         self.log = logging.getLogger(logger_name)
         self.log.addHandler(fh)
         self.log.setLevel(LOGGER_CONFIG["level"])
 
-    def update_rate(self, from_currency, to_currency):
-        self.log.info("Started update for: %s=>%s" % (from_currency, to_currency))
-        xrate = XRate.select().where(XRate.from_currency == from_currency,
-                                     XRate.to_currency == to_currency).first()
-
+    def update_rate(self, xrate):
+        self.log.info("Started update for: %s" % xrate)
         self.log.debug("rate before: %s", xrate)
         xrate.rate = self._update_rate(xrate)
         xrate.updated = peewee_datetime.datetime.now()
         xrate.save()
 
         self.log.debug("rate after: %s", xrate)
-        self.log.info("Finished update for: %s=>%s" % (from_currency, to_currency))
+        self.log.info("Finished update for: %s" % xrate)
 
     def _update_rate(self, xrate):
         raise NotImplementedError("_update_rate")
+
     def _send_request(self, url, method, data=None, headers=None):
         log = ApiLog(request_url=url, request_data=data, request_method=method,
                      request_headers=headers)
@@ -43,10 +50,11 @@ class _Api:
             self.log.exception("Error during request sending")
             log.error = str(ex)
             ErrorLog.create(request_data=data, request_url=url, request_method=method,
-                     error=str(ex), traceback=traceback.format_exc(chain=False))
+                            error=str(ex), traceback=traceback.format_exc(chain=False))
             raise
         finally:
             log.finished = peewee_datetime.datetime.now()
             log.save()
-    def _send(self, method, url, headers=None, data=None):
+
+    def _send(self, url, method, data=None, headers=None):
         return requests.request(method=method, url=url, headers=headers, data=data, timeout=HTTP_TIMEOUT)
